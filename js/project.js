96a3d1bd2143fa5d6c22f8c4f332b115b90c25fd
Jack Carpenter <lynxster222@gmail.com>
2016-07-31 22:58:05 -0400

    tried many variations. no good.
    tried to follow the jQuery exercise

--- js/project.js
+++ js/project.js
@@ -1,10 +1,34 @@
 $(document).ready(function(){
 //	$("nav").click(function(){
-		$(".current").mouseover(function(){
-			var current = $(this).attr("data-ipsum");
-			$(this).css({
-				"color" : "#F8FF73",
-			});
-		});
+		// $(".current").hover(function(){
+			// var current = $(this).attr("data-ipsum");
+			// $(this).css({
+				// "color" : "#F8FF73",
+			// });
+		// });
 //	});
- })
+
+	// $("nav").mouseenter(function() {
+		// ("current",data-ipsum);
+	// });
+// })
+
+// $("nav").click(function(){
+  // $(this).addClass(".current");
+// });
+	$("nav").hover(function(){
+	    $(this).css({
+	    	"color" : "#F8FF73",
+	    });
+	});
+});
+	
+
+
+
+
+
+
+
+
+
