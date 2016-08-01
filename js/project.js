e4c04dd3f42b7a01e2a15cdc9afb3efcd5e53be5
Jack Carpenter <lynxster222@gmail.com>
2016-08-01 08:12:39 -0400

    just trying to change the color on the nav a bar
    not sure of structure. need some direction

--- js/project.js
+++ js/project.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 //	$("nav").click(function(){
-		// $(".current").hover(function(){
-			// var current = $(this).attr("data-ipsum");
-			// $(this).css({
-				// "color" : "#F8FF73",
-			// });
-		// });
+// 		$(".current").hover(function(){
+//			 var current = $(this).attr("data-ipsum");
+//			 $(this).css({
+//				 "color" : "#F8FF73",
+//			 });
+//		 });
 //	});
 
 	// $("nav").mouseenter(function() {
@@ -16,12 +16,35 @@ $(document).ready(function(){
 // $("nav").click(function(){
   // $(this).addClass(".current");
 // });
-	$("nav").hover(function(){
-	    $(this).css({
-	    	"color" : "#F8FF73",
-	    });
-	});
-});
+	// $("nav").hover(function(){
+	    // $(this).css({
+	    	// "color" : "#F8FF73",
+	    // });
+	// });
+
+	// $(".thumb").click(function() {
+  // var imgSrc = $(this).attr("src");
+
+  // $("#bigimage").fadeOut(1000, function(){
+  		// 
+    	// $("#bigimage").attr("src", imgSrc);
+    	// $("#bigimage").fadeIn(1000);
+ //  });
+// });
+
+	$(".ipsum").click(function() {
+			var navBarSrc = $(this).attr(".current");
+			$("#submit").attr(".ipsum","navBarSrc");
+			$(this).css({
+				"color" : "red",
+			});
+
+			});
+	 });
+
+
+
+
 	
 
 
